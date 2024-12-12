17171fd8e577c0916c0043ffefd30cf4289e8932
Rabbi1089 <rabbi.ext@gmail.com>
2024-12-12 23:48:53 +0600

    Updated feature or fixed bug

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const jwt = require("jsonwebtoken");
 
 // middleware
 const corsOptions = {
-  origin: ["http://localhost:5173", "http://localhost:5174"],
+  origin: ["https://news-nexus-25.netlify.app/,  http://localhost:5173", "http://localhost:5174"],
   credentials: true,
   optionSuccessStatus: 200,
 };
